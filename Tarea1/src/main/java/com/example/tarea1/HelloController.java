0f651347f60963314d42575782759e0555088de3
arty4325 <arturo.acuna.duran@gmail.com>
2022-08-14 20:59:32 -0600

    Se agrego el tablero en el cual se va a desplegar la informacion

--- Tarea1/src/main/java/com/example/tarea1/HelloController.java
+++ Tarea1/src/main/java/com/example/tarea1/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.tarea1;
-
+import java.io.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.nio.Buffer;
 
 public class HelloController {
 
@@ -13,15 +16,20 @@ public class HelloController {
     private Button btn1;
 
     @FXML
-    public void SelectFile(MouseEvent event) {
+    public void SelectFile(MouseEvent event) throws IOException {
         FileChooser fc = new FileChooser();
         File selectedFile = fc.showOpenDialog(null);
 
         if (selectedFile != null) {
-            System.out.println(selectedFile.getAbsoluteFile());
-        } else {
-            System.out.println("No");
+            //System.out.println(selectedFile.getAbsoluteFile());
+            String path = selectedFile.getAbsolutePath();
+            String line = "";
+
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                //System.out.println(values[]);
+            }
         }
     }
-
 }
