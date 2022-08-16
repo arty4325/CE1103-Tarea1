8ea7aecc807343e37955de4063a4f0b4f7505c19
arty4325 <arturo.acuna.duran@gmail.com>
2022-08-15 21:51:39 -0600

    Se perfecciono el tablero y la clase estudiantes

--- Tarea1/src/main/java/com/example/tarea1/HelloController.java
+++ Tarea1/src/main/java/com/example/tarea1/HelloController.java
@@ -1,19 +1,81 @@
 package com.example.tarea1;
 import java.io.*;
+
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
 import java.nio.Buffer;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
 
     @FXML
     private Button btn1;
+
+    @FXML
+    private AnchorPane table;
+
+    @FXML
+    private TableView<Student> Table; // Lo que se le dice a la tabla es que contenga estudiantes
+
+    @FXML
+    private TableColumn<Student, String> Carne;
+
+    @FXML
+    private TableColumn<Student, String> Correo;
+
+    @FXML
+    private TableColumn<Student, String> Nikname;
+
+    @FXML
+    private TableColumn<Student, String> Nombre;
+
+    @FXML
+    private TableColumn<Student, String> NotaFinal;
+
+    @FXML
+    private TableColumn<Student, String> PromedioExQT;
+
+    @FXML
+    private TableColumn<Student, String> PromedioExamenes;
+
+    @FXML
+    private TableColumn<Student, String> PromedioProyectos;
+
+    @FXML
+    private TableColumn<Student, String> PromedioQuices;
+
+    @FXML
+    private TableColumn<Student, String> PromedioTareas;
+
+    @FXML
+    private TableColumn<Student, String> Proyecto1;
+
+    @FXML
+    private TableColumn<Student, String> Proyecto2;
+
+    @FXML
+    private TableColumn<Student, String> Proyecto3;
+
+    @FXML
+    private TableColumn<Student, String> Telefono;
+
+    @FXML
+    private TableColumn<Student, String> Tipo;
 
     @FXML
     public void SelectFile(MouseEvent event) throws IOException {
@@ -28,8 +90,39 @@ public class HelloController {
             BufferedReader br = new BufferedReader(new FileReader(path));
             while((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                //System.out.println(values[]);
+                System.out.println(values[0]);
             }
         }
+    }
+
+    ObservableList<Student> list = FXCollections.observableArrayList(
+            new Student("2022049304", "oacuna@estudiantec.cr", "arty4325", "Oscar Arturo", "77", "69","90", "80", "80", "90", " 80", "90", "80", "70769040", "A")
+    );
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        Carne.setCellValueFactory(new PropertyValueFactory<Student, String>("Carne"));
+        Correo.setCellValueFactory(new PropertyValueFactory<Student, String>("Correo"));
+        Nikname.setCellValueFactory(new PropertyValueFactory<Student, String>("Nikname"));
+        Nombre.setCellValueFactory(new PropertyValueFactory<Student, String>("Nombre"));
+        NotaFinal.setCellValueFactory(new PropertyValueFactory<Student, String>("NotaFinal"));
+        PromedioExQT.setCellValueFactory(new PropertyValueFactory<Student, String>("PromedioExQT"));
+        PromedioExamenes.setCellValueFactory(new PropertyValueFactory<Student, String>("PromedioExamenes"));
+        PromedioProyectos.setCellValueFactory(new PropertyValueFactory<Student, String>("PromedioProyectos"));
+        PromedioQuices.setCellValueFactory(new PropertyValueFactory<Student, String>("PromedioQuices"));
+        PromedioTareas.setCellValueFactory(new PropertyValueFactory<Student, String>("PromedioTareas"));
+        Proyecto1.setCellValueFactory(new PropertyValueFactory<Student, String>("Proyecto1"));
+        Proyecto2.setCellValueFactory(new PropertyValueFactory<Student, String>("Proyecto2"));
+        Proyecto3.setCellValueFactory(new PropertyValueFactory<Student, String>("Proyecto3"));
+        PromedioProyectos.setCellValueFactory(new PropertyValueFactory<Student, String>("PromedioProyectos"));
+        Telefono.setCellValueFactory(new PropertyValueFactory<Student, String>("Telefono"));
+        Tipo.setCellValueFactory(new PropertyValueFactory<Student, String>("Tipo"));
+
+
+        Table.setItems(list);
+
+
+
+
     }
 }
---
+++ Tarea1/src/main/java/com/example/tarea1/Student.java
@@ -0,0 +1,98 @@
+package com.example.tarea1;
+
+public class Student {
+
+    private String Carne;
+    private String Correo;
+    private String Nikname;
+    private String Nombre;
+    private String NotaFinal;
+    private String PromedioExQT;
+    private String PromedioExamenes;
+    private String PromedioQuices;
+    private String PromedioTareas;
+    private String PromedioProyectos;
+    private String Proyecto1;
+    private String Proyecto2;
+    private String Proyecto3;
+    private String Telefono;
+    private String Tipo; // todos estos son los atributos del programa
+
+    public Student(String carne, String correo, String nikname, String nombre, String notaFinal, String promedioExQT, String promedioExamenes, String promedioQuices, String promedioTareas, String promedioProyectos, String proyecto1, String proyecto2, String proyecto3, String telefono, String tipo) {
+        Carne = carne;
+        Correo = correo;
+        Nikname = nikname;
+        Nombre = nombre;
+        NotaFinal = notaFinal;
+        PromedioExQT = promedioExQT;
+        PromedioExamenes = promedioExamenes;
+        PromedioQuices = promedioQuices;
+        PromedioTareas = promedioTareas;
+        PromedioProyectos = promedioProyectos;
+        Proyecto1 = proyecto1;
+        Proyecto2 = proyecto2;
+        Proyecto3 = proyecto3;
+        Telefono = telefono;
+        Tipo = tipo;
+    }
+
+    public String getCarne() {
+        return Carne;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public String getNikname() {
+        return Nikname;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getNotaFinal() {
+        return NotaFinal;
+    }
+
+    public String getPromedioExQT() {
+        return PromedioExQT;
+    }
+
+    public String getPromedioExamenes() {
+        return PromedioExamenes;
+    }
+
+    public String getPromedioQuices() {
+        return PromedioQuices;
+    }
+
+    public String getPromedioTareas() {
+        return PromedioTareas;
+    }
+
+    public String getPromedioProyectos() {
+        return PromedioProyectos;
+    }
+
+    public String getProyecto1() {
+        return Proyecto1;
+    }
+
+    public String getProyecto2() {
+        return Proyecto2;
+    }
+
+    public String getProyecto3() {
+        return Proyecto3;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+}
